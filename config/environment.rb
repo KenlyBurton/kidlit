583b3e52d43245ba43361a44f9061355ae5564fc
Kenly <kenly@Kenlys-MacBook-Air.local>
2017-02-04 09:46:34 -0500

    update config

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 require 'bundler'
 Bundler.require
+# The block of code below configures out database in development - we'll use a sqlite3 database that's stored in a folder called "db"
 configure :development do
-  set :database, "sqlite3:db/database.db"
+ set :database, "sqlite3:db/database.db"
 end
-
 configure :production do
-  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
 end
